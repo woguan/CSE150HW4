b360aea3c39adc1f1ce72429625b5c681867849d
user.email <woguan@ucsd.edu>
2016-11-20 22:52:11 -0800

    Final

--- StarterCodePA4/BayesianNetwork.py
+++ StarterCodePA4/BayesianNetwork.py
@@ -82,7 +82,7 @@ class BayesianNetwork(object):
     def performRejectionSampling(self, queryVar, givenVars, numSamples):
         """ generated source for method performRejectionSampling """
         #  TODO
-
+        
         mySample = Sample()
         myList = {}
 
@@ -92,43 +92,29 @@ class BayesianNetwork(object):
         	sampleNode(myList)
 
         def sampleNode(nodeList):
-        	#print self.varMap[node].getVariable().getName() #working
-        	#print "current node: ", node.getName()
-        	#assignment = {}
-        	#assignment["Cloudy"] = True
-        	#assignment["Rain"] = True
-        	#assignment["wetGrass"] = True
-        	#print self.varMap[node].getProbability(assignment, True)
-        	#print "Look: "
-        	#print self.rootNodes[0].getProbability(assignment, True)
-        	#print self.rootNodes[0].getVariable().getName()
         	myCounterDebug = 0
         	allSet = False
         	childList = {}
-        	#First step, giving T/F for roots. They do not have parents.
+        	#First step, giving T/F for roots. They do not have parents so we can assign probability for them.
         	for nodes in nodeList:
-        		childList[nodes] = True #adding to the list
-        		#print "asdadsasdasd: ", nodes.getName()
-        		if (random.random() <= self.varMap[nodes].getProbability(nodes.getName(), True)):
+        		childList[nodes] = True #adding to the list as true - we add them just to to tell they are visited already
+        		if (random.random() <= self.varMap[nodes].getProbability(nodes.getName(), True)): #generate random & assign probability
         			mySample.setAssignment(nodes.getName(), True)
         		else:
         			mySample.setAssignment(nodes.getName(), False)
-        		for children in self.varMap[nodes].getChildren():
-        			#print children.getVariable().getName()
-        			childList[children.getVariable()] = False
+        		for children in self.varMap[nodes].getChildren(): # add the childs in list as false. It means childs are not visited
+        			if children.getVariable() not in childList: #make sure we have no duplicates
+        				childList[children.getVariable()] = False
 
-        	#print "The value is: ", mySample.getValue("Cloudy")
 
         	while (allSet is False): #keep doing until childList's elements are all true
-        		myCounterDebug += 1 # for debug, used when infinite loop happens
         		listAdd = [] # hold variables to add to childlist
         		for c in childList: # for each children of childList
         			if (childList[c]): # if this child is done... skip
 						continue
         			isValid = True # boolean to check if parents are defined
         			for parents in self.varMap[c].getParents(): # check if all parents is defined
-        				#print "MY PARENT IS.... = ", parents.getVariable().getName() #--- YES! REMOVE LATER
-        				if (parents.getVariable() not in childList): #if not i list.. skip
+        				if (parents.getVariable() not in childList): #if parent is not in list.. skip
         					isValid = False
         				else: # if in list... but not defined... also skip
         					if (childList[parents.getVariable()]):
@@ -137,83 +123,35 @@ class BayesianNetwork(object):
         						isValid = False
 
         			if (isValid): # if all parents is defined
-        				#print "IS VALID OK?!?!?!"
-        				childList[c] = True # child 'c' is done
-        				#print c.getName()
-        				assigns = {} # adding assignments
-        				for parents in self.varMap[c].getParents():
+        				childList[c] = True # set child 'c' is done
+        				assigns = {} # hold assignments
+        				for parents in self.varMap[c].getParents(): #filling up assignments
         					assigns[parents.getVariable().getName()] = mySample.getValue(parents.getVariable().getName())
-        					#print "THIS: ", parents.getVariable().getName()
-        				myRan = random.random()
-        				#print "Random is: ", myRan
-        				#for pp in assigns:
-        				#	print "Name: ", pp, "Value: ", assigns[pp]
-        				#print "c = ", c.getName()
-        				#print "self.varMap[c].getProbability(assigns, True) = ", self.varMap[c].getProbability(assigns, True)
+        				myRan = random.random() #generate random
         				if (myRan <= self.varMap[c].getProbability(assigns, True)): #applying sampling
         					mySample.setAssignment(c.getName(), True) 
-        					#print "SETTING TRUE"
         				else:
-        					#print "SETTING FALSE"
         					mySample.setAssignment(c.getName(), False)
 
         				for children in self.varMap[c].getChildren(): #keep track of children to be added
-        					#print "IM ADDING THIS: ", children.getVariable().getName()
-        					#childList[children.getVariable()] = False
-        					listAdd += [children.getVariable()] 
-        				isValid = True #reset isValid
+        					listAdd += [children.getVariable()]
+        		#add children of children to list. Avoiding duplicates			
         		for e in listAdd:
         			if (e not in childList):
         				childList[e] = False
 
-        		isAllChecked = True		
+        		isAllChecked = True # boolean to check if all variables are visited		
         		for ch in childList: #check if all members are defined
         			if (childList[ch] is False):
         				isAllChecked = False
         		allSet = isAllChecked
 
-        		#if (allSet):
-        		#	print "THIS HAS FINISHED... CHECKING VALUES...."
-        		#	for ch in childList:
-        		#		print "AAAA: ", ch.getName(), "BBBB: ", childList[ch]
-
-
-
-
-        		if (myCounterDebug > 10):
-        			print "BROKEN....."
-        			for c in childList:
-        				print c.getName()
-        			break
-        #print "FINISH...."
-
-
-
-
-
-        #givenVars is a array that contains objects of type RandomVariable located at Assignment4.py
-        #Thus... can use their methods.
-        #for objs in givenVars:
-        #	print objs.getName()
-        
-        #this prints all nodes in the graph
-        #for objs in self.varMap:
-        #	print objs.getName()
-        
-        
         counter = [0.0, 0.0]
-        #priorSample()
-
-        #for n in self.varMap:
-        #	
         
         for i in range(numSamples):
         	priorSample()
-        	#for n in self.varMap:
-        	#	print "Name: ", n.getName(), "value: ", mySample.getValue(n.getName())
         	isValid = True
         	for g in givenVars:
-
         		if mySample.getValue(g.getName()) != givenVars[g]:
         			isValid = False
         	if (isValid):
@@ -223,18 +161,9 @@ class BayesianNetwork(object):
         		else:
         			counter[1] += 1.0
 
-        	
-        
-
-        #print "c1 = ", counter[0], "c2 = ", counter[1]
-
         if (counter[0] + counter[1] == 0):
         	return 0
 
-        #if (mySample.getValue(queryVar.getName())):
-      #  	return counter[0]/(counter[0] + counter[1])
-      #  else:
-       # 	return counter[1]/(counter[0] + counter[1])
         return counter[0]/(counter[0] + counter[1])
    
         
@@ -251,14 +180,10 @@ class BayesianNetwork(object):
         mySample = Sample()
         myList = {}
 
-        def weightedSample(givenNodes): #givenNodes has its name&value! asd[Sprinker ] = Ture
-        	#weight
-        	mySample.setWeight(1.0) # For weight
-        	w = 1.0
+        def weightedSample(givenNodes):
+        	w = 1.0 #weight
         	for n in self.rootNodes:
         		myList[n.getVariable()] = False
-        	
-        	myCounterDebug = 0 #For debug only -- can be removed later
         	allSet = False # boolean to check if all childList is visited
         	childList = {}
         	#First step, giving T/F for roots.
@@ -266,10 +191,8 @@ class BayesianNetwork(object):
         		childList[node] = True #adding to the list
         		#check if current node has evidence... if not, do like rejected sample by generating random number
         		if node in givenNodes: # has evidence
-        			#print "this is in evidence: ", node.getName()
         			if (givenNodes[node]):
         				mySample.setAssignment(node.getName(), True)
-        				
         				w *= self.varMap[node].getProbability(node.getName(),True)
         			else:
         				mySample.setAssignment(node.getName(), False)
@@ -280,20 +203,18 @@ class BayesianNetwork(object):
         			else:
         				mySample.setAssignment(node.getName(), False)
         		for children in self.varMap[node].getChildren(): #add childs to childlist
-        			#print children.getVariable().getName()
-        			childList[children.getVariable()] = False
-        	#NEXT STEP
-
+        			if children.getVariable() not in childList: # avoid duplicate
+        				childList[children.getVariable()] = False
+        	
+        	#NEXT STEP...
         	while (allSet is False): #keep doing until childList's elements are all true
-        		myCounterDebug += 1 # for debug, used when infinite loop happens
         		listAdd = [] # hold variables to add to childlist
-        		for c in childList: # for each children of childList
+        		for c in childList: # for each child of childList
         			if (childList[c]): # if this child is done... skip
 						continue
         			isValid = True # boolean to check if parents are defined
         			for parents in self.varMap[c].getParents(): # check if all parents is defined
-        				#print "MY PARENT IS.... = ", parents.getVariable().getName() #--- YES! REMOVE LATER
-        				if (parents.getVariable() not in childList): #if not in list.. skip
+        				if (parents.getVariable() not in childList): #if parent is not in list.. skip
         					isValid = False
         				else: # if in list... but not defined... also skip
         					if (childList[parents.getVariable()]):
@@ -302,71 +223,42 @@ class BayesianNetwork(object):
         						isValid = False
 
         			if (isValid): # if all parents is defined
-        				#print "IS VALID OK?!?!?!"
         				childList[c] = True # child 'c' is done
-        				#print c.getName()
-        				assigns = {} # adding assignments
-        				for parents in self.varMap[c].getParents():
+        				assigns = {} # variable for assignments
+        				for parents in self.varMap[c].getParents(): #filling assignments
         					assigns[parents.getVariable().getName()] = mySample.getValue(parents.getVariable().getName())
-        					#print "THIS: ", parents.getVariable().getName()
         				myRan = random.random() #random for rejected if needed
-        				#print "Random is: ", myRan
-        				#print "BELOW IS ASSIGNMENTS"
-        				#for pp in assigns:
-        				#	print "Name: ", pp, "Value: ", assigns[pp]
-        				#print "ASSIGNMENTS DONE...."
-        				#print "c = ", c.getName()
-        				#print "self.varMap[c].getProbability(assigns, True) = ", self.varMap[c].getProbability(assigns, True)
 
-        				if c in givenNodes: #if has evidence
-        				#	print "HAS EVIDENCE: ", c.getName()
+        				if c in givenNodes: #if c has evidence
         					if givenNodes[c]:
         						mySample.setAssignment(c.getName(), True)
         						w *= self.varMap[c].getProbability(assigns,True)
-        				#		print "SETTING TRUE... weight = ", self.varMap[c].getProbability(assigns,True)
         					else:
         						mySample.setAssignment(c.getName(), False)
         						w *= self.varMap[c].getProbability(assigns,False)
-        				#		print "SETTING FALSE... weight = ", self.varMap[c].getProbability(assigns,False)
         				else: #if no evidence...
-        				#	print "NO EVIDENCE...."
         					if (myRan <= self.varMap[c].getProbability(assigns, True)): #applying sampling
         						mySample.setAssignment(c.getName(), True) 
-        				#		print "SET true...."
         					else:
         						mySample.setAssignment(c.getName(), False)
-        				#		print "SET FALSE...."
 
-        				for children in self.varMap[c].getChildren(): #keep track of children to be added
-        					#print "IM ADDING THIS: ", children.getVariable().getName()
-        					#childList[children.getVariable()] = False
+        				for children in self.varMap[c].getChildren(): #keep track of children of child to be added
         					listAdd += [children.getVariable()] 
-        				isValid = True #reset isValid -- maybe not necessary
 
-        		for e in listAdd:
+        		for e in listAdd: #adding children of child. Avoiding duplicates
         			if (e not in childList):
         				childList[e] = False
 
-        		isAllChecked = True		
+        		isAllChecked = True	#boolean to check if all variables were visited	
         		for ch in childList: #check if all members are defined
         			if (childList[ch] is False):
         				isAllChecked = False
         		allSet = isAllChecked
 
-        		"""if (allSet):
-        			print "THIS HAS FINISHED... CHECKING VALUES...."
-        			for ch in childList:
-        				print "AAAA: ", ch.getName(), "BBBB: ", childList[ch]"""
         		if (allSet):
         			return w
-
-
-
-        #print weightedSample(givenVars)
-        #return 0
         weights = [0, 0]
         
-
         for n in range(numSamples):
         	w = weightedSample(givenVars)
         	if (mySample.getValue(queryVar.getName())):
@@ -377,12 +269,6 @@ class BayesianNetwork(object):
 
         if (weights[0] + weights[1] == 0):
         	return 0
-
-        #print "weight[0] = ", weights[0], " weight[1] = ", weights[1]
-        #if (mySample.getValue(queryVar.getName())):
-        	#return weights[0] / (weights[0] + weights[1])
-       # else:
-        #	return weights[1] / (weights[0] + weights[1])
         return weights[0] / (weights[0] + weights[1])
     # 
     #     * Returns an estimate of P(queryVal=true|givenVars) using Gibbs sampling
@@ -394,11 +280,6 @@ class BayesianNetwork(object):
     def performGibbsSampling(self, queryVar, givenVars, numTrials):
         """ generated source for method performGibbsSampling """
         #  TODO
-
-
-        #queryNode, queryValue = queryVar[0]
-
-        #print "Q: ", queryNode.getName(), " V: ", queryValue
         mySample = Sample()
         
         noEvidenceList = []
@@ -406,67 +287,54 @@ class BayesianNetwork(object):
         #assigning evidences to sample
         for givenNode in givenVars:
         	mySample.setAssignment(givenNode.getName(), givenVars[givenNode])
-        	#print "Setting ", givenNode.getName(), " to be ", givenVars[givenNode]
 
+        #filling up noEvidenceList with non evidence variables
         for node in self.varMap:
         	if node not in givenVars:
         		noEvidenceList += [node]
-        		#print "added: ", node.getName(), "to non-evidence list"
 
+        #giving a random value for non evidence variables
         for node in noEvidenceList:
         	rand = random.random()
         	if (rand < 0.5):
-        		#print "assigning randomly", node.getName(), " to be True"
         		mySample.setAssignment(node.getName(), True)
         	else:
-        		#print "assigning randomly", node.getName(), " to be False"
         		mySample.setAssignment(node.getName(), False)
 
 
         count = [0,0]
-
-        #print "queryNode is: ", queryVar.getName()
-
-        #print "This is the list of Sample: "
-        #for dd in self.varMap:
-        	#print dd.getName(), " = ", mySample.getValue(dd.getName())
-        #print "Sample list done."
         
         for runs in range(numTrials):	#repeat n times
-        	if (mySample.getValue(queryVar.getName())):
+        	if (mySample.getValue(queryVar.getName())): # count[0] keep track of queryVar = True
         		count[0] += 1.0
         	else:
-        		count[1] += 1.0
-        	for noEvidenceNode in noEvidenceList:
-        		assignList = {}
-        		#print "Getting list of Assignments: "
+        		count[1] += 1.0 # count[1] keep track of queryVar = False
+        	for noEvidenceNode in noEvidenceList: # for each no-evidence variable...
+        		assignList = {} # preparing to add assignments
 
-        		for node in self.varMap: #filling up assignments
+        		for node in self.varMap: #filling up assignments defined previously
         			assignList[node.getName()] = mySample.getValue(node.getName())
-        			#print node.getName(), " = ", mySample.getValue(node.getName())
-        		#print len(assignList)
-        		#print noEvidenceNode.getName()
+
         		ptrue = self.varMap[noEvidenceNode].getProbability(assignList, True)
         		pfalse = self.varMap[noEvidenceNode].getProbability(assignList, False)
-        		for child in self.varMap[noEvidenceNode].getChildren():
-        			#print child.getVariable().getName()
-        			assignList[noEvidenceNode.getName()] = True
-        			pt = self.varMap[child.getVariable()].getProbability(assignList, True)
-        			assignList[noEvidenceNode.getName()] = False
-        			pf = self.varMap[child.getVariable()].getProbability(assignList, True)
 
+        		for child in self.varMap[noEvidenceNode].getChildren(): #for each child of the current no-evidence variable
         			if mySample.getValue(child.getVariable().getName()):
-        				ptrue *= pt
-        				pfalse *= pf
+        				assignList[noEvidenceNode.getName()] = True # set current no-evidence variale = True
+        				ptrue *= self.varMap[child.getVariable()].getProbability(assignList, True)
+        				assignList[noEvidenceNode.getName()] = False# set current no-evidence variale = False
+        				pfalse *= self.varMap[child.getVariable()].getProbability(assignList, True)
         			else:
-        				ptrue *= (1.0-pt)
-        				pfalse *= (1.0-pf)
-        		P = ptrue/(ptrue+pfalse)
-        		rann = random.random()
-        		if rann <= P:
+        				assignList[noEvidenceNode.getName()] = True # set current no-evidence variale = True
+        				ptrue *= self.varMap[child.getVariable()].getProbability(assignList, False)
+        				assignList[noEvidenceNode.getName()] = False# set current no-evidence variale = False
+        				pfalse *= self.varMap[child.getVariable()].getProbability(assignList, False)
+        		P = ptrue/(ptrue+pfalse) #Normalize
+        		rann = random.random() # Generate random
+        		if rann <= P: #assigning new value for the current noEvidenceNode
         			mySample.setAssignment(noEvidenceNode.getName(), True)
         		else:
         			mySample.setAssignment(noEvidenceNode.getName(), False)
-        #print "count[0] = ", count[0], " count[1] = ", count[1]
+        if count[0]+count[1] == 0
+        	return 0
         return count[0]/(count[1]+count[0])
-        #return 0
